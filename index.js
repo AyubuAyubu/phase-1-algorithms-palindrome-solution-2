ea63e578ef7b7b1249332eb669082da72f3402cc
AyubuAyubu <ayubu6143@gmail.com>
2022-08-25 11:43:57 +0300

    Test not passing

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let re = /[^A-Za-z0-9]/g
+  str = str.toLowerCase().replace(re, '')
+  var varlen = str.length
+  for (var x = 0; x < varlen / 2; x++) {
+    if (str[x] !== str[varlen - 1 - x]) {
+      return false
+      
+    }
+  }
+  return true
 }
+
 
 /* 
   Add your pseudocode here
@@ -8,6 +19,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 // You can run `node index.js` to view these console logs
